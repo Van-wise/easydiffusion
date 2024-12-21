83bb0b077bd674f0d32d2dee545e118d9a8ba8a8
Van-wise <fxmidiot@foxmail.com>
2024-12-21 11:08:36 +0800

    Update check_modules.py

--- scripts/check_modules.py
+++ scripts/check_modules.py
@@ -34,7 +34,7 @@ modules_to_check = {
     "sqlalchemy": "2.0.19",
     "python-multipart": "0.0.6",
     # "xformers": "0.0.16",
-    "huggingface-hub": "0.27.0",
+    "huggingface-hub": "0.23.5",
     "wandb": "0.13.7",
 }
 modules_to_log = ["torch", "torchvision", "sdkit", "stable-diffusion-sdkit", "diffusers"]
