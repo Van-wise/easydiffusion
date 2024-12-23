28fe8c0dd4e72bb217c9e902ed07e4adc922d950
Van-wise <fxmidiot@foxmail.com>
2024-12-23 14:34:39 +0800

    Update check_modules.py

--- scripts/check_modules.py
+++ scripts/check_modules.py
@@ -23,10 +23,10 @@ modules_to_check = {
     "torch": ("1.11.0", "1.13.1", "2.0.0", "2.0.1"),
     "torchvision": ("0.12.0", "0.14.1", "0.15.1", "0.15.2"),
     "setuptools": "69.5.1",
-    # "sdkit": "2.0.15.6", # checked later
-    "diffusers": "0.21.4", # checked later
+    "sdkit": "2.0.20.4", # checked later
+    "diffusers": "0.28.2", # checked later
     "stable-diffusion-sdkit": "2.1.5",
-    "rich": "12.6.0",
+    #"rich": "12.6.0",
     "uvicorn": "0.19.0",
     "fastapi": "0.115.6",
     #"pycloudflared": "0.2.0",
